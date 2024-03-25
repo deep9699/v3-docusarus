26e4dcb76091322d31c0a8dd018093537f4ceb2c
deep-kristal <deepbhavsar9@gmail.com>
2024-03-25 11:38:15 +0530

    added check for linkes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,9 @@ const config = {
   organizationName: "facebook", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "warn",
+  onBrokenAnchors: "warn",
+  onBrokenMarkdownLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
